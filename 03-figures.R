ebdf8e56e0619550c5ad2384a655f7611adef9a3
juanrocha <aguilajk@gmail.com>
2021-09-07 16:52:27 +0200

    update git ignore, analysis and figs scripts

---
+++ 03-figures.R
@@ -0,0 +1,180 @@
+library(tidyverse)
+library(patchwork)
+
+#### Figure 1: controllability of individual RS ####
+load("data_processed/individualRS_control.RData")
+
+g1 <- df_comb %>% 
+    ungroup() %>% 
+    group_by(regime_shift, type) %>% 
+    summarize(n = n()) %>% 
+    ungroup() %>% group_by(regime_shift) %>% 
+    mutate(total = sum(n),
+           regime_shift = as_factor(regime_shift)) %>% 
+    mutate(regime_shift = fct_rev(regime_shift)) %>% #%>% pull(regime_shift) %>% levels()
+    ggplot(aes(regime_shift, n)) +
+    geom_col(aes(fill = type), position = "stack") +
+    coord_flip() +
+    scico::scale_fill_scico_d(
+        palette = "berlin", name = "Controllability", 
+        labels = c("Feedback vertex set", "Minimum vertex set")) +
+    labs(x = "Number of driving nodes", y = "Regime shifts", tag = "A") +
+    theme_light(base_size = 7) + theme(legend.position = "bottom")
+
+
+g2 <- df_comb %>% 
+    ungroup() %>% 
+    group_by(regime_shift, type) %>% 
+    summarize(n = n()) %>% 
+    ungroup() %>% 
+    left_join(df_stats) %>% 
+    group_by(regime_shift) %>% 
+    mutate(total = sum(n),
+           regime_shift = as_factor(regime_shift),
+           prop = n / nodes) %>% 
+    mutate(regime_shift = fct_rev(regime_shift)) %>% 
+    ggplot(aes(prop, regime_shift )) +
+    geom_col(aes(fill = type), position = "stack") +
+    scico::scale_fill_scico_d(
+        palette = "berlin", name = "Controllability", 
+        labels = c("Feedback vertex set", "Minimum vertex set")) +
+    scale_x_continuous(labels = scales::percent) +
+    labs(x = "Proportion of nodes", y = "", tag = "B") +
+    theme_light(base_size = 7) + 
+    theme(legend.position = "bottom", axis.text.y = element_blank())
+
+
+## J210826: The order of factors is not working, not sure what am doing wrong. I manage to organize them but not to show the higher values on the top (it used to work with fct_reorder)
+g3 <- df_comb %>% 
+    ungroup() %>% 
+    group_by(node, type) %>% 
+    summarize(n = n(), .groups = "keep") %>% 
+    pivot_wider(names_from = type, values_from = n) %>% 
+    mutate(total = sum(fvs, mds, na.rm = TRUE)) %>% 
+    arrange(desc(total)) %>% 
+    filter(total > 3) %>% 
+    mutate(node = as_factor(node)) %>% 
+    mutate(node = fct_reorder(.f = node, .x = total, .desc = FALSE) %>% fct_rev()) %>%
+    #pull(node) %>% fct_inorder()
+    pivot_longer(cols = c("fvs", "mds"), names_to = "type", values_to = "n") %>% 
+    ggplot(aes(n, fct_inorder(node))) +
+    geom_col(aes(fill = type), position = "stack") +
+    scico::scale_fill_scico_d(
+        palette = "berlin", name = "Controllability", 
+        labels = c("Feedback vertex set", "Minimum vertex set")) +
+    labs(x = "Number of regime shifts", y = "Control variables", tag = "C") +
+    theme_light(base_size = 7) + theme(legend.position = "bottom")
+
+(g1 + g2 + g3) / guide_area() + plot_layout(guides = "collect", heights = c(3,0.5))
+
+
+ggsave(
+    filename = "fig1_control-individua-rs.png",
+    path = "paper/figures/",
+    plot = last_plot(),
+    device = "png",
+    width = 7, height = 3,
+    units = "in", dpi = 450,
+    bg = "white"
+)
+
+
+#### Figure 2: controllability in coupled RS ####
+load("data_processed/cascading_control.RData")
+
+p1 <- g2$data %>% 
+    ggplot(aes(prop, regime_shift )) +
+    geom_col(aes(fill = type), position = "stack") +
+    scico::scale_fill_scico_d(
+        palette = "berlin", name = "Controllability", 
+        labels = c("Feedback vertex set", "Minimum vertex set"),
+        guide = guide_legend(nrow = 2,
+            title.position = 'top', 
+            keywidth = unit(2.5, "mm"),
+            keyheight = unit(2.5, "mm"))) +
+    scale_x_reverse(labels = scales::percent) +
+    scale_y_discrete(position = "right") +
+    labs(x = "Ratio of nodes", y = "", tag = "A") +
+    theme_light(base_size = 5) + 
+    theme(legend.position = "bottom", 
+          axis.text.y.right = element_text(hjust = 1))
+
+# this graph has 
+p2 <- df_comb %>% 
+    select(-n) %>% 
+    group_by(couple) %>% 
+    add_count() %>% ungroup() %>% 
+    separate(couple, into = c("rs1", "rs2"), sep = "_")  %>% 
+    nest(cols = c(node, type)) %>% 
+    arrange(rs1, rs2) %>% 
+    mutate(rs1 = as_factor(rs1)  ,
+           rs2 = as_factor(rs2) %>% fct_rev() ) %>% 
+    ggplot(aes(rs1, rs2)) +
+    geom_tile(aes(fill = n)) +
+    scico::scale_fill_scico(
+        name = "Number of nodes", palette = "roma", direction = -1,
+        guide = guide_colorbar(
+            title.hjust = 0.5, direction = "horizontal",
+            title.position = "top", barheight = unit(2.5, "mm"), 
+            barwidth = unit(2, "cm"))) +
+    labs(x= "Regime shift couples", y = "", tag = "B") +
+    theme_light(base_size = 5) +
+    theme(axis.text.x = element_text(angle=90, hjust = 1, vjust = 0.5),
+          axis.text.y = element_blank(), 
+          legend.position = "bottom")
+
+## this is proportion, p2 is number of nodes
+p3 <- df_stats %>% 
+    mutate(rs1 = fct_rev(rs1)) %>% 
+    ggplot(aes(rs1, rs2)) +
+    geom_tile(aes(fill = prop)) +
+    scico::scale_fill_scico(
+        name = "Ratio of nodes", palette = "vikO", direction = 1,
+        guide = guide_colorbar(
+            title.hjust = 0.5, direction = "horizontal",
+            title.position = "top", barheight = unit(2.5, "mm"), 
+            barwidth = unit(2, "cm"))) +
+    labs(x= "Regime shift couples", y = "", tag = "C") +
+    theme_light(base_size = 5) +
+    theme(axis.text.x = element_text(angle=90, hjust = 1, vjust = 0.5),
+          axis.text.y = element_blank(), 
+          legend.position = "bottom")
+
+
+p4 <- df_comb %>% 
+    ungroup() %>% group_by(node) %>% 
+    select(node) %>% 
+    summarise(n = n()) %>% 
+    arrange(desc(n)) %>% 
+    #filter(n>100) %>% 
+    slice_max(order_by = n, n = 30) %>% 
+    mutate(node = as_factor(node),
+           node = fct_reorder(node, n, sort)) %>% 
+    ggplot(aes(node, n)) +
+    geom_col() +
+    coord_flip() +
+    labs(x = "Most common driver nodes", y = "Regime shifts pair-wise combinations", tag = "D")+
+    theme_light(base_size = 5)
+
+
+layout <- "
+ABBCCDD
+ABBCCDD
+ABBCCDD
+ABBCCDD
+#####DD
+"
+
+p1 + p2 + p3 + p4 + 
+    plot_layout(design = layout, heights = c(2,2,2,2,0.1))
+
+ggsave(
+    filename = "fig2_combined-control.png",
+    path = "paper/figures/",
+    plot = last_plot(),
+    device = "png",
+    width = 7, height = 3.5,
+    dpi = 500, units = "in",
+    bg = "white"
+)
+    
