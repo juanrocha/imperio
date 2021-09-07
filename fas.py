0443bbc2eb7e3841108a3b20fcd4915df5cd1bcd
juanrocha <aguilajk@gmail.com>
2021-09-07 16:54:55 +0200

    deleting some old files

--- fas.py
+++ fas.py
@@ -2,7 +2,7 @@ from igraph import *
 import pandas as pd
 
 def feedback_set(dat):
-    edgelist = list(dat[["Tail", "Head"]].itertuples(index=False, name = None))
+    edgelist = list(dat[["tail", "head"]].itertuples(index=False, name = None))
     net = Graph.TupleList(edges = edgelist, directed = True)
     fas = Graph.feedback_arc_set(net)
     return fas
